e1ba32e391de0c1264008419818698d1ce02f90f
Elias Bello <eabelloc.dev@gmail.com>
2022-08-15 23:24:37 +0200

    neoland-events_LISTO-LISTO

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-/*1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
-evento click que ejecute un console log con la información del evento del click*/
-const button = document.querySelector("#btnToClick")
-button.innerHTML("Click")
-
-button.addEventListener("click", (e) => {
+//1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+//evento click que ejecute un console log con la información del evento del click
+const button = document.querySelector("#btnToClick");
+button.innerHTML ="Click";
+button.addEventListener("click",(e)=>{
     console.log(e)
 });
 
+/*const click = document.querySelector(".click");
+click.addEventListener("click", (click) => {
+    console.log(click)
+})*//*No entiendo esta parte del ejercicio, o sea, tengo que atacar al input con class="click"? O al boton?*/
+ 
 
-/*1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.*/
+//1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 const focus = document.querySelector(".focus")
-
-focus.addEventListener("focus", (Elias)=> {
-    console.log(Elias)
+focus.addEventListener("focus",(ver) => {
+    console.log(ver)
 })
 
 
-
-
-/*1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.*/
+//1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.*/
+const value = document.querySelector(".value")
+value.addEventListener("input", (escribir) => {
+    console.log(escribir)
+})
 
